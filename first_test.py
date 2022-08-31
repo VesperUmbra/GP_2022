abc8dbb27ad826a0e334d4edba11fd0d0b2eefaa
VesperUmbra <vesper_umbra@qq.com>
2022-08-31 14:31:02 +0800

    Initial setting

--- first_test.py
+++ first_test.py
@@ -1,1 +1,37 @@
+
+#测试各个软件包
+import virtualenv
+import tqdm
+import json
+import demjson
+import pickle
+import argparse
+import numpy
+import scipy
+import pandas
+import matplotlib
+import seaborn
+import pyecharts
+import requests
+import urllib
+import beautifulsoup4
+
+import scrapy
+import selenium
+import jieba
+import gensim
+import networkx
+import Pillow
+import opencv_python
+
+import librosa
+import statsmodels
+import scikit_learn
+
+import pymongo
+import pymysql
+
 print("2022 autumn GP by czw")
+a=int(input())
+b=int(input())
+print(a+b)
